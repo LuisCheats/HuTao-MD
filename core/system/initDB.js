4352095322767172e6d0fd718831f6229f263a5b
LuisCheats <luiselcrack183@gmail.com>
2026-04-25 20:32:15 -0500

    update initDB.js

--- core/system/initDB.js
+++ core/system/initDB.js
@@ -11,10 +11,10 @@ function initDB(m, client) {
 settings.nameid ??= "𝓗𝓾𝓣𝓪𝓸-𝓜𝓓 𝓒𝓱𝓪𝓷𝓷𝓮𝓵"
 settings.type ??= 'Owner'
 settings.link ??= 'https://celest.qzz.io'
-settings.banner ??= 'https://raw.githubusercontent.com/LuisCheats/DATABASE/refs/heads/main/1766201596136.jpg'
-settings.icon ??= 'https://raw.githubusercontent.com/LuisCheats/DATABASE/refs/heads/main/1766201596136.jpg'
+settings.banner ??= 'https://raw.githubusercontent.com/LuisCheats/DATABASE/refs/heads/main/1767585523669.jpg'
+settings.icon ??= 'https://raw.githubusercontent.com/LuisCheats/DATABASE/refs/heads/main/1767585523669.jpg'
 settings.currency ??= 'Yenes'
-settings.namebot ??= 'Hu Tao'
+settings.namebot ??= '𝗛𝘂 𝗧𝗮𝗼'
 settings.botname ??= '𝓗𝓾𝓣𝓪𝓸 - 𝓜𝓓'
 settings.owner ??= ''
 
