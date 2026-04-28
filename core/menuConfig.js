da908344d80da04fbbaef9893061953f1e401a3b
LuisCheats <luiselcrack183@gmail.com>
2026-04-27 23:16:48 -0500

    update

---
+++ core/menuConfig.js
@@ -0,0 +1,42 @@
+export const mainMenuImage = 'https://file.garden/ae-9DPf0ekWVe7ex/TikVid.io_7633242534839356692-hd.mp4';
+export const categoryImages = {
+  anime: 'https://i.pinimg.com/736x/53/13/9a/53139a45b8a098588a4e1b6557ee8492.jpg',
+  downloads: 'https://i.pinimg.com/736x/60/64/bb/6064bb9466503fd9e752f8db55d92ced.jpg',
+  economia: 'https://pbs.twimg.com/media/Fi_HBmFUAAY6L9i.jpg',
+  gacha: 'https://img.anmosugoi.com/file/media-sugoi/2023/06/Hatsune-Miku-celebra-sus-16-anos-con-un-festival-de-sorpresas-1.jpg',
+  grupo: 'https://e0.pxfuel.com/wallpapers/459/137/desktop-wallpaper-the-vocaloid-crew-suits-crew-hatsune-miku-rin-and-len-kagamine-vocaloid-people-team-anime-group-megurine-luka.jpg',
+  nsfw: 'https://i.pinimg.com/736x/95/92/63/95926316fbc8708df3245d45fabf3393.jpg',
+  owner: 'https://jrocknews.com/wp-content/uploads/2019/03/hatsune-miku-v4x-promo-top.jpg',
+  profile: 'https://i.pinimg.com/736x/cf/4f/bd/cf4fbdccb346330efd7f02c60f52c6d0.jpg',
+  sockets: 'https://i.pinimg.com/736x/46/8d/e3/468de3ae91716d0b8033fc2b0d85772f.jpg',
+  stickers: 'https://i.pinimg.com/236x/7f/f1/04/7ff10431e8ab905b86498cbe94a0dbf1.jpg',
+  utils: 'https://e1.pxfuel.com/desktop-wallpaper/532/937/desktop-wallpaper-hatsune-miku-1920x1080-hatsune-miku-thumbnail.jpg'
+};
+
+export const categoryAliases = {
+  anime: ['anime', 'reacciones'],
+  downloads: ['downloads', 'descargas'],
+  economia: ['economia', 'economy', 'eco'],
+  gacha: ['gacha', 'rpg'],
+  grupo: ['grupo', 'group'],
+  nsfw: ['nsfw', '+18'],
+  owner: ['owner', 'dueño', 'creador'],
+  profile: ['profile', 'perfil'],
+  sockets: ['sockets', 'bots', 'config'],
+  stickers: ['stickers', 'sticker'],
+  utils: ['utils', 'utilidades', 'herramientas']
+};
+
+export const categoryNames = {
+  anime: 'ANIME',
+  downloads: 'DESCARGAS',
+  economia: 'ECONOMÍA',
+  gacha: 'GACHA',
+  grupo: 'GRUPO',
+  nsfw: 'NSFW',
+  owner: 'OWNER',
+  profile: 'PERFIL',
+  sockets: 'CONFIG',
+  stickers: 'STICKERS',
+  utils: 'UTILIDADES'
+};
