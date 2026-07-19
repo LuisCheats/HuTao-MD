62a6ef1920d358e773e2f98443b3c5e891d309e9
luisxitq <luiselcrack183@gmail.com>
2026-07-19 02:15:38 -0500

    update

--- cmds/stickers/sticker.js
+++ cmds/stickers/sticker.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { spawn } from 'child_process';
 import fetch from 'node-fetch';
-import exif from '../../core/exif.js';
+import exif from '../../lib/exif.js';
 const { writeExif } = exif;
 
 export default {
@@ -21,7 +21,7 @@ export default {
       const name = user.name;
       const meta1 = user.metadatos ? String(user.metadatos).trim() : '';
       const meta2 = user.metadatos2 ? String(user.metadatos2).trim() : '';
-      let texto1 = meta1 ? meta1 : `Ig: Lzzin.xyz`;
+      let texto1 = meta1 ? meta1 : `ᴋᴀᴏʀᴜᴋᴏ ✌🏻 Wᴀʙᴏᴛ`;
       let texto2 = meta1 ? (meta2 ? meta2 : '') : `@${name}`;
       let urlArg = null;
       let argsWithoutUrl = [];
